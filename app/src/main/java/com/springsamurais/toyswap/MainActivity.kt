8b19e1d0006bf1385550367042217d1b04156154
Sam Kettle <sam.kettle3@icloud.com>
2024-06-26 13:51:17 +0100

    Front end app initialised

---
+++ app/src/main/java/com/springsamurais/toyswap/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.springsamurais.toyswap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
