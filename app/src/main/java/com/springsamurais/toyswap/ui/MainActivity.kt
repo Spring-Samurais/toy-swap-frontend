48c1dd126bb53e1efc639ee72e56dafee099b6a2
Sam Kettle <sam.kettle3@icloud.com>
2024-06-27 12:03:47 +0100

    feat: create necessary packages

--- app/src/main/java/com/springsamurais/toyswap/ui/MainActivity.kt
+++ app/src/main/java/com/springsamurais/toyswap/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.springsamurais.toyswap
+package com.springsamurais.toyswap.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.springsamurais.toyswap.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
