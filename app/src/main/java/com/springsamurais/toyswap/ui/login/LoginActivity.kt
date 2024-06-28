6b540c5f4106e7c12e69d38f2fdd625bf357d89a
Sam Kettle <sam.kettle3@icloud.com>
2024-06-28 23:27:59 +0100

    feat: start login logic, add data classes for response and request

--- app/src/main/java/com/springsamurais/toyswap/ui/login/LoginActivity.kt
+++ app/src/main/java/com/springsamurais/toyswap/ui/login/LoginActivity.kt
@@ -2,11 +2,33 @@ package com.springsamurais.toyswap.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.springsamurais.toyswap.R
+import com.springsamurais.toyswap.ui.login.data.LoginRequest
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val usernameEditText = findViewById<EditText>(R.id.username_input)
+        val passwordEditText = findViewById<EditText>(R.id.password_input)
+        val loginButton = findViewById<Button>(R.id.login_button)
+
+        loginButton.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val password = usernameEditText.text.toString()
+
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please complete all fields", Toast.LENGTH_SHORT).show()
+            } else {
+                login(username, password)
+            }
+        }
+    }
+
+    private fun login(username: String, password: String) {
     }
 }
