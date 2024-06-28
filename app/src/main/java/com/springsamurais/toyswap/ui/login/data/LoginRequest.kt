ade0c49e38be41f3007a647421211dc0e930e201
Sam Kettle <sam.kettle3@icloud.com>
2024-06-28 23:27:59 +0100

    feat: start login logic, add data classes for response and request

---
+++ app/src/main/java/com/springsamurais/toyswap/ui/login/data/LoginRequest.kt
@@ -0,0 +1,3 @@
+package com.springsamurais.toyswap.ui.login.data
+
+data class LoginRequest(val username: String, val password: String)
---
+++ app/src/main/java/com/springsamurais/toyswap/ui/login/data/LoginResponse.kt
@@ -0,0 +1,3 @@
+package com.springsamurais.toyswap.ui.login.data
+
+data class LoginResponse(val token: String, val userId: Int)
