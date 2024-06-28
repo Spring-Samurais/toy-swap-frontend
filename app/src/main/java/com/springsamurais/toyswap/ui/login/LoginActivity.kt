ecc48d38c84fd20d80d47f56d9f590fb3ca099f7
Sam Kettle <sam.kettle3@icloud.com>
2024-06-28 22:39:53 +0100

    feat: draft activity_login.xml created with LoginActivity view initialised

---
+++ app/src/main/java/com/springsamurais/toyswap/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.springsamurais.toyswap.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.springsamurais.toyswap.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
