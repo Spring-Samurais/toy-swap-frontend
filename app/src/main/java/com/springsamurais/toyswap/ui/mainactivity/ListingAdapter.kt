6971953af10b74f650687a898e4e35e61b96f9e1
Sam Kettle <sam.kettle3@icloud.com>
2024-07-01 14:55:05 +0100

    feat: add date from API to listing and formatting

--- app/src/main/java/com/springsamurais/toyswap/ui/mainactivity/ListingAdapter.kt
+++ app/src/main/java/com/springsamurais/toyswap/ui/mainactivity/ListingAdapter.kt
@@ -8,17 +8,18 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.springsamurais.toyswap.R
 import com.springsamurais.toyswap.model.Listing
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ListingAdapter(private val listings: List<Listing>) : RecyclerView.Adapter<ListingAdapter.ViewHolder>() {
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val listingImage: ImageView
         val listingName: TextView
-//        val locationIcon: ImageView
         val locationText: TextView
-//        val userIcon: ImageView
         val usernameText: TextView
         val description: TextView
+        val dateText: TextView
 
 
         init {
@@ -27,6 +28,7 @@ class ListingAdapter(private val listings: List<Listing>) : RecyclerView.Adapter
             locationText = view.findViewById(R.id.location_text)
             usernameText = view.findViewById(R.id.username_text)
             description  = view.findViewById(R.id.description_text)
+            dateText     = view.findViewById(R.id.date_posted)
         }
     }
 
@@ -39,10 +41,15 @@ class ListingAdapter(private val listings: List<Listing>) : RecyclerView.Adapter
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         val currentListing: Listing = listings[position]
 
-        viewHolder.listingName.text = currentListing.title
+        // Format the date in a user-friendly way
+        val date: Date? = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.ENGLISH).parse(currentListing.datePosted!!)
+        val formattedDate: String = SimpleDateFormat("dd-MM-yy", Locale.ENGLISH).format(date!!)
+
+        viewHolder.listingName.text  = currentListing.title
         viewHolder.locationText.text = currentListing.member!!.location
         viewHolder.usernameText.text = currentListing.member!!.nickname
-        viewHolder.description.text = currentListing.description
+        viewHolder.description.text  = currentListing.description
+        viewHolder.dateText.text     = formattedDate
 
         viewHolder.listingImage.setImageResource(R.drawable.pikachu_toy)
     }
