64cafbe8dab94ca44c1b90048dc0444e634dd33c
Sam Kettle <sam.kettle3@icloud.com>
2024-07-01 12:37:18 +0100

    feat: ListingRepository can retrieve live data from GET request

--- app/src/main/java/com/springsamurais/toyswap/model/ListingRepository.kt
+++ app/src/main/java/com/springsamurais/toyswap/model/ListingRepository.kt
@@ -1,4 +1,31 @@
 package com.springsamurais.toyswap.model
 
-class ListingRepository {
+import android.app.Application
+import androidx.lifecycle.MutableLiveData
+import com.springsamurais.toyswap.service.APIService
+import com.springsamurais.toyswap.service.RetrofitInstance
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class ListingRepository(app: Application) {
+
+    var data = MutableLiveData<List<Listing>>()
+
+    fun getData() : MutableLiveData<List<Listing>> {
+        val listService : APIService = RetrofitInstance.instance
+        var call: Call<List<Listing>> = listService.getListings()
+
+        call.enqueue(object: Callback<List<Listing>> {
+            override fun onResponse(call: Call<List<Listing>>, response: Response<List<Listing>>) {
+                val retrievedListings = response.body()
+                data.value = retrievedListings
+            }
+
+            override fun onFailure(p0: Call<List<Listing>>, p1: Throwable) {
+                // Stretch task: implement failure logic
+            }
+        })
+        return data
+    }
 }
