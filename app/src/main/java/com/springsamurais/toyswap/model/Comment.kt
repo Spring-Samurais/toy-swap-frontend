d5fda72436a54105dbd1769c140be15922725aaa
Sam Kettle <sam.kettle3@icloud.com>
2024-07-04 11:36:22 +0100

    feat: refactor Comment model

--- app/src/main/java/com/springsamurais/toyswap/model/Comment.kt
+++ app/src/main/java/com/springsamurais/toyswap/model/Comment.kt
@@ -2,22 +2,10 @@ package com.springsamurais.toyswap.model
 
 import java.util.Date
 
-class Comment {
-
-    private var commentId: Long? = null;
-    private var text: String? = null;
-    private var commenter: Member? = null;
-    private var listing: Listing? = null;
-    private var dateCommented: Date? = null;
-
-    constructor()
-    constructor(commentId: Long?, text: String?, commenter: Member?, listing: Listing?, dateCommented: Date?) {
-        this.commentId = commentId
-        this.text = text
-        this.commenter = commenter
-        this.listing = listing
-        this.dateCommented = dateCommented
-    }
-
-
-}
+data class Comment(
+    var commentId: Long?,
+    var text: String? ,
+    var commenter: Member?,
+    var listing: Listing?,
+    var dateCommented: Date?
+)
