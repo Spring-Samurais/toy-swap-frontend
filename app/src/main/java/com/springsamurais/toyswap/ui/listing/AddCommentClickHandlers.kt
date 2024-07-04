2b1f86805d7a6a77bfa5dfdb02466695476e5cff
Sam Kettle <sam.kettle3@icloud.com>
2024-07-04 22:43:51 +0100

    feat: click handlers for adding comments implemented

--- app/src/main/java/com/springsamurais/toyswap/ui/listing/AddCommentClickHandlers.kt
+++ app/src/main/java/com/springsamurais/toyswap/ui/listing/AddCommentClickHandlers.kt
@@ -5,16 +5,14 @@ import android.content.Intent
 import android.view.View
 import com.springsamurais.toyswap.ui.mainactivity.MainActivity
 
-class ViewListingClickHandlers(private var context: Context) {
+class AddCommentClickHandlers(private var context: Context) {
 
     fun addCommentButtonClicked(view: View) {
-        val intent = Intent(view.context, AddCommentActivity::class.java)
-        context.startActivity(intent)
+
     }
 
-    fun backButtonClicked(view: View) {
+    fun cancelButtonClicked(view: View) {
         val intent = Intent(view.context, MainActivity::class.java)
         context.startActivity(intent)
     }
-
 }
