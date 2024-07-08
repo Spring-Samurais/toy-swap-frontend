57ef3ea0c952d512a80ffe66bc7ff2496d2d6fe7
mmoazam <14061746+mmoazam@users.noreply.github.com>
2024-07-08 09:52:15 +0100

    added empty UpdateListingActivity.kt this updated the other files automatically

---
+++ app/src/main/java/com/springsamurais/toyswap/ui/updatelisting/UpdateListingActivity.kt
@@ -0,0 +1,12 @@
+package com.springsamurais.toyswap.ui.updatelisting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.springsamurais.toyswap.R
+
+class UpdateListingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_listing)
+    }
+}
